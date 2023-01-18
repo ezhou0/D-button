400cf56130208e7c8e437b8517d9637835c4da80
Edwin <edwinzhou99@gmail.com>
2023-01-17 16:31:12 -0800

    add button component

---
+++ src/component/Button/Button.jsx
@@ -0,0 +1,17 @@
+import React , {useEffect} from 'react';
+import './button.css';
+
+const Button = () =>{
+    return(
+        <>
+            <div className='button'>
+                <h1 className="button-text">
+                    Button
+                </h1>
+            </div>
+        </>
+        
+    )
+}
+
+export default Button;
